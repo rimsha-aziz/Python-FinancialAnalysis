a91cbf7085cb5815f33576c87d31d25f634d05fa
Raziz <60328896+rimsha-aziz@users.noreply.github.com>
2020-02-26 20:22:04 -0500

    Changed main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,62 +1,66 @@
-
+#import dependencies 
 import os
 import csv 
 
-csv_file = os.path.join("..","PyBank","Budget_Data.csv")
-
-with open (csv_file,'r') as csvfile: 
-    csvreader=csv.reader(csvfile,delimiter=',') 
-
-    monthtotal = 0 
-    net_total = 0 
-    date = []
-    PL = []
-    total_change = 0 
-
+bank_csv = os.path.join('budget_data.csv')
+#read csv file and define values 
+with open(bank_csv, "r") as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=',')
+    date = [] #list for date column 
+    #starting values 
+    net_total = 0
+    months = 0
+    pl = [] #list for profit/loss colum
+    transactional = [] #list to hold the difference
+    #skip header 
     header = next(csvreader)
-
+    #for loop to moves through rows 
     for row in csv.reader(csv_file):
+        #append row of list 
         date.append(row[0])
-        monthtotal = monthtotal + 1  
-       
-
-        net_total.append(int(row[1]))
+        transactional.append(row[1])
+        #calculae total profit/loss
         net_total = net_total + (int(row[1]))
+        #calculate months 
+        months = months + 1
 
-    for x,y in zip(PL,PL[1]):
-        PL.append(int(y) - int(x))
-        total_change = sum(PL)
-        average = total_change/monthtotal
+    #zip so that the profit/loss change can be calculated 
+    for x, y in zip(transactional, transactional[1:]):
+        #Append to calculate the profit/loss change 
+        pl.append(int(y) - int(x))
 
-greatest_inc = max(PL)
-greatest_dec = min(PL)
+        total_change = sum(pl)
+        average = total_change / months
+        average = "{:.2f}".format(average) # formatting average 
 
+#The greatest increase or decrease in profits (date and amount) 
+greatest_increase = max(pl) #getting max
+monthinc = pl.index(greatest_increase) + 1  # geting index of max 
+monthdateinc = date[monthinc] # getting month associated with index of max 
+greatest_decrease = min(pl) #getting min 
+monthdec = pl.index(greatest_decrease) + 1  #getting index of min 
+monthdatedec = date[monthdec] #getting month associated with index of min 
+
+#Print out of results 
 print("Financial Analysis")
 print("------------------------")
-print("Total months: " + str(monthtotal))
-print("Total volume: " + str(net_total))
+print("Total months: " + str(months))
+print("Total volume: $" + str(net_total))
 #The average change in "Profit/Losses" between months over the entire period
-print("Average monthly change: "+ str(average))
-#The greatest increase in profits 
-print("Greatest increase: " + str(greatest_inc))
-#The greatest decrease in losses 
-print("Greatest decrease: " + str(greatest_dec))
+print("Average monthly change: $"+ str(average))
+#The greatest increase in profits (date and amount) over the entire period
+print("Greatest increase: $" + str(greatest_increase)  + str(monthdateinc))
+#The greatest decrease in losses (date and amount) over the entire period
+print("Greatest decrease: $" + str(greatest_decrease)  + str(monthdatedec))
 
-##Output to text file
-text_file = open("PYBankFinancials.txt", "w")
-text_file.write("Financial Analysis\n")
+#Output to text file
+text_file = open("PYBankResults.txt", "w")
+text_file.write("Financial Analysis\n") # \n to signify end of line 
 text_file.write("------------------------\n")
-text_file.write("Total months: " + str(monthtotal) + "\n")
-text_file.write("Total volume: " + str(net_total) + "\n")
-text_file.write("Average monthly change: " + str(average) + "\n")
-text_file.write("Greatest increase: " + str(greatest_inc) + "\n")
-text_file.write("Greatest decrease: " + str(greatest_de) + "\n")
-text_file.close()          
-        
-
-
-
-        
-
-
-
+text_file.write("Total months: " + str(months) + "\n")
+text_file.write("Total volume: $" + str(net_total) + "\n")
+text_file.write("Average monthly change: $" + str(average) + "\n")
+text_file.write("Greatest increase: " + str(greatest_increase) + "%" + str(monthdateinc) + "\n")
+text_file.write("Greatest decrease: " + str(greatest_decrease) + "%" + str(monthdatedec) + "\n")
+#close file 
+text_file.close() 
